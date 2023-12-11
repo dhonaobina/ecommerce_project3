b19a94141d5bcaf9f703a1bf6c5fb47162a6ea5b
dhonaobina <dhona.obina@outlook.com>
2023-12-11 20:55:27 +1300

    positioning updated for product items and thumbnails for mobile and large screen

--- src/App.js
+++ src/App.js
@@ -8,48 +8,62 @@ function Header() {
   return (
     <>
       <header className="flex items-center justify-between p-8 border-b border-slate-400 max-w-7xl mx-auto">
-      <div className="flex items-center justify-start gap-4">
-        <img src={logo} alt=""></img>
+        <div className="flex items-center justify-start gap-4">
+          <img src={logo} alt=""></img>
 
-        <nav>
+          <nav>
+            <ul className="flex items-center justify-start gap-4">
+              <li>Collections</li>
+              <li>Men</li>
+              <li>Women</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </div>
+
+        <div>
           <ul className="flex items-center justify-start gap-4">
-            <li>Collections</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>
+              <button>
+                <IoCartOutline />
+              </button>
+            </li>
+            <li>
+              <img src={avatar} alt="" className="w-12" />
+            </li>
           </ul>
-        </nav>
-      </div>
-
-      <div>
-        <ul className="flex items-center justify-start gap-4">
-          <li><button><IoCartOutline /></button></li>
-          <li><img src={avatar} alt="" className="w-12"></img></li>
-        </ul>
-      </div>
+        </div>
       </header>
     </>
-  )
+  );
 }
 
 function App() {
-  const [products] = useState(data)
-
+  const [products] = useState(data);
+  const[value,setValue] = useState(1)
+  
+  const {mainImage} = products[value]
+  
   return (
     <>
       <Header />
 
-      <section>
-        {products.map((item) => (
-          <article key={item.id}>
-            <img src={item.mainImage} alt="" />
+      <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10">
+        <article>
+          <img src={mainImage} alt="" className="h-96"/>
 
-          <ul>
-            <img src={item.thumbnail} alt="" />
+          <ul className="flex items-center justify-start gap-3 flex-wrap">
+            {products.map((item, index) => ( 
+              <li key={item.id} onClick={() => setValue(index)}>
+                <img src={item.thumbnail} alt="" className="w-20" />
+              </li>
+            ))}
           </ul>
-          </article>
-          ))}
+        </article>
+        <article>
+          <h1>lorem epsum a diam maecenas</h1>
+        </article>
       </section>
     </>
   );
